4402f53776755f9f9355d7966ff674fbdcb4461a
Galina <galina.hvan1011@gmail.com>
2018-04-18 12:14:54 +0700

    added webpack configuration for project 3

--- Project 3 - Recipes/src/js/index.js
+++ Project 3 - Recipes/src/js/index.js
@@ -1,1 +1,9 @@
 // Global app controller
+import str1 from './models/Search';
+
+console.log(str1);
+
+// import { add as a, multiply } from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(searchView.add(5,6));
